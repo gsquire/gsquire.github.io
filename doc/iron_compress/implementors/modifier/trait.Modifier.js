4ed3953204510321ee389f4bce9f68ffeb50b800
Garrett Squire <garrettsquire@gmail.com>
2016-06-06 16:40:25 -0700

    add compress documentation

---
+++ doc/iron_compress/implementors/modifier/trait.Modifier.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['iron_compress'] = ["impl&lt;'a&gt; Modifier&lt;<a class='struct' href='http://ironframework.io/core/iron/iron/response/struct.Response.html' title='iron::response::Response'>Response</a>&gt; for <a class='struct' href='iron_compress/struct.DeflateWriter.html' title='iron_compress::DeflateWriter'>DeflateWriter</a>&lt;'a&gt;","impl&lt;'a&gt; Modifier&lt;<a class='struct' href='http://ironframework.io/core/iron/iron/response/struct.Response.html' title='iron::response::Response'>Response</a>&gt; for <a class='struct' href='iron_compress/struct.GzipWriter.html' title='iron_compress::GzipWriter'>GzipWriter</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
