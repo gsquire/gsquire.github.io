9855c024363fa415e0428bc67d3022f898fc1a40
Garrett Squire <garrettsquire@gmail.com>
2016-06-14 13:52:12 -0700

    update reroute docs

---
+++ doc/reroute/implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['reroute'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='reroute/struct.Router.html' title='reroute::Router'>Router</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
